ce21afbb9905e244e9879baf0dc069c8593c9c85
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2020-12-18 12:40:16 -0800

    Testimonial Arrow Fnctionality

--- app/js/script.js
+++ app/js/script.js
@@ -2,7 +2,13 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body')
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade')
+const fadeElems = document.querySelectorAll('.has-fade');
+
+const leftArrow = document.querySelector('#leftArrow');
+const rightArrow = document.querySelector('#rightArrow');
+const radioButtons = document.querySelectorAll('.testimonials__radioButtons')
+const slidesNavigation = document.querySelector('.testimonials__slidesNavigation');
+let cardIndex = 0;
 
 btnHamburger.addEventListener('click', function () {
   console.log('open');
@@ -21,6 +27,48 @@ btnHamburger.addEventListener('click', function () {
       element.classList.remove('fade-out');
       element.classList.add('fade-in');
     });
-      
   }  
 });
+
+slidesNavigation.addEventListener('click', function(e) {
+  console.log(e.target);
+  cardIndex = Number(e.target.id.split('-').pop()) - 1;
+  console.log(cardIndex);
+  updateArrows();
+})
+
+leftArrow.addEventListener('click', function() {
+  if(cardIndex < 3) {
+    cardIndex++;
+    radioButtons[cardIndex].checked = true;
+    updateArrows()
+  }
+})
+
+rightArrow.addEventListener('click', function() {
+  if(cardIndex > 0) {
+    cardIndex--;
+    radioButtons[cardIndex].checked = true;
+    updateArrows()
+  }
+})
+
+
+function updateArrows() {
+  if(cardIndex === 0) {
+    leftArrow.src = './images/arrowleft.png';
+    leftArrow.style.transform = 'scaleX(1)';
+    rightArrow.src = './images/arrowright.png';
+    rightArrow.style.transform = 'scaleX(1)';
+  } else if(cardIndex > 0 && cardIndex < 3) {
+    leftArrow.src = './images/arrowleft.png';
+    leftArrow.style.transform = 'scaleX(1)';
+    rightArrow.src = './images/arrowleft.png';
+    rightArrow.style.transform = 'scaleX(-1)';
+  } else if(cardIndex === 3) {
+    leftArrow.src = './images/arrowright.png';
+    leftArrow.style.transform = 'scaleX(-1)';
+    rightArrow.src = './images/arrowleft.png';
+    rightArrow.style.transform = 'scaleX(-1)';
+  }
+}
